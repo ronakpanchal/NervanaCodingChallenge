96e5403231fc1b8f07756d56b64dd3e8e3a9e6ab
Ronak Panchal <ronakpnchl@gmail.com>
2017-02-24 23:15:12 -0500

    changes for file_data parameter
    changes for file_data parameter

--- command_parser.py
+++ command_parser.py
@@ -18,28 +18,37 @@ from Task import process_shell_command
 logging.basicConfig(level=logging.INFO, filename='applogs.log', filemode='w', format='%(name)s %(levelname)s %(message)s')
 logger = logging.getLogger('app_logger') 
 
-def get_valid_commands(queue, fi):
+def get_valid_commands(queue, fi, file_data):
+    logger.info('Inside get valid commands')    
+    commands =set()
+    valid_commands =set()
+
+    if file_data:
+	commands = set(file_data['COMMAND_LIST'])
+	valid_commands = set(file_data['VALID_COMMANDS'])
+	# logger.info('List of commands from post request is ', commands)
+	# logger.info('List of vali commands from post request is ', valid_commands)
+    else:
     # TODO: efficiently evaluate commands
-    file_config = configparser.ConfigParser(allow_no_value=True, strict=False, delimiters="*")
-    file_config.read(fi)
-    sections = file_config.sections()
-    commands = set()
-    valid_commands = set()
-    logger.info("Available sections in given configuration file are {}".format(sections))
-    logger.info(" List of keys in  COMMAND_LIST are :")
-    logger.info("-----------------------------------------------------")
-    for key in file_config['COMMAND LIST']:
-        logger.info(key)
-        commands.add(key)
-    logger.info("-----------------------------------------------------")
-    logger.info(" List of keys in  VALID_COMMANDS are :")
-    for key in file_config['VALID COMMANDS']:
-        logger.info(key)
-        valid_commands.add(key)
-    logger.info("------------------------------------------------------")
-    commands_to_ptocess = commands.intersection(valid_commands)
+    	file_config = configparser.ConfigParser(allow_no_value=True, strict=False, delimiters="*")
+    	file_config.read(fi)
+    	sections = file_config.sections()
+    	valid_commands = set()
+    	logger.info("Available sections in given configuration file are {}".format(sections))
+    	logger.info(" List of keys in  COMMAND_LIST are :")
+    	logger.info("-----------------------------------------------------")
+    	for key in file_config['COMMAND LIST']:
+        	logger.info(key)
+       		commands.add(key)
+    	logger.info("-----------------------------------------------------")
+   	logger.info(" List of keys in  VALID_COMMANDS are :")
+    	for key in file_config['VALID COMMANDS']:
+        	logger.info(key)
+        	valid_commands.add(key)
+    	logger.info("------------------------------------------------------")
+    commands_to_process = commands.intersection(valid_commands)
     logger.info("List of valid commands are as follows")
-    for command in commands_to_ptocess:
+    for command in commands_to_process:
         logger.info(command)
         queue.put(command)
     logger.info("------------------------------------------------------")
